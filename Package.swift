7efd964a47d9e3177877b8c683b6f551b86b7d3d
runner <runner@Mac-1686547293786.local>
2023-06-12 06:04:47 +0000

    OpenSSL 1.1.1u (1.1.2100)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/duckduckgo/OpenSSL-XCFramework/releases/download/1.1.2000/OpenSSL.xcframework.zip",
-            checksum: "f04692e30ddbc42882f601b77759c160f6613ecdc595a9ad18891f79d87d1ac6"
+            url: "https://github.com/duckduckgo/OpenSSL-XCFramework/releases/download/1.1.2100/OpenSSL.xcframework.zip",
+            checksum: "ef52041cf05ee39b5c4845ed09236e2f76b53c01741b514bf310274815ef291c"
         )
     ]
 )
