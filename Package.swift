e8bca74a7c8b3c9368a682b4f7bd367cca6ef6fa
runner <runner@Mac-1714369437894.local>
2024-04-29 06:06:04 +0000

    Add visionOS to supported platforms https://github.com/krzyzanowskim/OpenSSL/pull/205 (3.1.5003)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenSSL",
-            url: "https://github.com/duckduckgo/OpenSSL-XCFramework/releases/download/3.1.5001/OpenSSL.xcframework.zip",
-            checksum: "a14289d910560cb1fd7d4d908802189628ddfa9adc8129a4ac790b0f3026d275"
+            url: "https://github.com/duckduckgo/OpenSSL-XCFramework/releases/download/3.1.5003/OpenSSL.xcframework.zip",
+            checksum: "8be5e5fc9856d65103f9cb3704edbeaa0e518f5101d6428b1730575d00522359"
         )
     ]
 )
